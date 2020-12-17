50d2a69dea4253c6e31a18fe15a8b21937e0a215
rehandwi <rehandwi03@gmail.com>
2020-12-17 14:51:15 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("shaun");
+  const [person, setPerson] = useState({
+    name: "mario",
+    age: 40,
+  });
+
+  const clickHandler = () => {
+    setName("chun-li");
+    setPerson({ name: "rehan", age: 21 });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name} </Text>
+      <Text>
+        His name is {person.name} and his age is {person.age}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update State" onPress={clickHandler} />
+      </View>
     </View>
   );
 }
@@ -14,8 +30,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
